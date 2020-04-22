b88ec3e7f21c77cd8737a3c5adafbab1b9c36b20
Dennis Jensen <ngh1adh95@hotmail.com>
2020-04-22 12:24:55 +0200

    Slider and results

--- Final-Project/calibration_test.py
+++ Final-Project/calibration_test.py
@@ -14,7 +14,7 @@ images_conveyor = np.asarray([images_left_conveyor, images_right_conveyor]).T
 images = np.asarray([images_left, images_right]).T
 
 calibrate = False
-debug_disp_slider = True
+debug_disp_slider = False
 
 
 nb_vertical = 6
@@ -30,16 +30,17 @@ else:
 images = images_conveyor[0]
 
 # Stereo Class
-min_disparity = 1  # 2
-num_disparity = 16 * 8  # 160
-block_size = 5  # 9
+min_disparity = 1  # 1
+num_disparity = 75  # 160
+block_size = 9  # 5
 disp12MaxDiff = 130 # 200
 uniqRatio = 1 # 2
 speckleRange = 20 # 2
 speckleWindow = 125 # 30
 preFilter = 30 # 30
-p1 = 8 * 3 * block_size**2
-p2 = 32 * 3 * block_size**2
+p1 = 100
+p2 = 400
+mode = cv2.STEREO_SGBM_MODE_HH
 
 stereo = cv2.StereoSGBM_create(numDisparities=num_disparity,
                                blockSize=block_size,
@@ -49,32 +50,28 @@ stereo = cv2.StereoSGBM_create(numDisparities=num_disparity,
                                speckleRange=speckleRange,
                                speckleWindowSize=speckleWindow,
                                preFilterCap=preFilter,
-                               P1=p1,
-                               P2=p2)
+                               mode=mode)
 
-
-images_conveyor = images_conveyor[100:500]  # start from first box with 80:
+images_conveyor = images_conveyor[0:-1]  # start from first box with 80:
 
 for images in images_conveyor:
     left_img, right_img = Cal.remapImagesStereo(images, random=False, debug=False)
     # images = np.concatenate((left_img, right_img), axis=1)
-    left_img = downsample_image(left_img, 0.6)
-    right_img = downsample_image(right_img, 0.6)
+    left_img = downsample_image(left_img, 0.4)
+    right_img = downsample_image(right_img, 0.4)
+    
     if debug_disp_slider:
         slider = Slider(stereo, left_img, right_img)
         slider.create_slider()
 
-    # left_img = cv2.GaussianBlur(left_img, (3, 3), 0)
-    # right_img = cv2.GaussianBlur(right_img, (3, 3), 0)
+    disparity_img = stereo.compute(left_img, right_img).astype(np.float32) / 16.0
+    norm_disparity_img = cv2.normalize(disparity_img, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX,
+                                       dtype=cv2.CV_32F)
 
-#     disparity_img = stereo.compute(left_img, right_img).astype(np.float32) / 16.0
-#     norm_disparity_img = cv2.normalize(disparity_img, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX,
-#                                        dtype=cv2.CV_32F)
-# #
-#     cv2.imshow('Images', norm_disparity_img)
-#     if cv2.waitKey(10) & 0xFF == ord('q'):
-#         break
-#
-# cv2.destroyAllWindows()
+    cv2.imshow('Images', norm_disparity_img)
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        break
+
+cv2.destroyAllWindows()
 
 # export_pointcloud(disparity_map=disparity_img, colors=left_img)
--- Final-Project/lib/sliderProgram.py
+++ Final-Project/lib/sliderProgram.py
@@ -21,19 +21,24 @@ class Slider():
                 cv2.getTrackbarPos('disp12MaxDiff', 'Disparity'))
         self.stereo.setMinDisparity(
             cv2.getTrackbarPos('minDisparity', 'Disparity'))
-        # self.stereo.setNumDisparities(
-        #     cv2.getTrackbarPos('numDisparities', 'Disparity'))
+        self.stereo.setSpeckleWindowSize(
+            cv2.getTrackbarPos('speckleWindowSize', 'Disparity'))
+        self.stereo.setNumDisparities(
+            cv2.getTrackbarPos('numDisparities', 'Disparity'))
         self.stereo.setPreFilterCap(
             cv2.getTrackbarPos('preFilterCap', 'Disparity'))
+        # P1 = 3 * 8 * cv2.getTrackbarPos('blockSize', 'Disparity')
+        # P2 = 3 * 32 * cv2.getTrackbarPos('blockSize', 'Disparity')
+        # self.stereo.setP1(P1)
+        # self.stereo.setP2(P2)
         self.stereo.setP1(
             cv2.getTrackbarPos('P1', 'Disparity'))
         self.stereo.setP2(
             cv2.getTrackbarPos('P2', 'Disparity'))
 
         disparity = self.stereo.compute(self.image_l, self.image_r).astype(np.float32) / 16.0
-
         cv2.imshow('Disparity',
-                   (disparity - self.stereo.getMinDisparity()) / self.stereo.getNumDisparities())
+                   (disparity - self.stereo.getMinDisparity()) / cv2.getTrackbarPos('numDisparities', 'Disparity'))
 
     def create_slider(self):
         cv2.namedWindow('Disparity')
@@ -42,11 +47,11 @@ class Slider():
         cv2.createTrackbar('speckleWindowSize', 'Disparity', self.stereo.getSpeckleWindowSize(), 200, self.update)
         cv2.createTrackbar('speckleRange', 'Disparity', self.stereo.getSpeckleRange(), 50, self.update)
         cv2.createTrackbar('disp12MaxDiff', 'Disparity', self.stereo.getDisp12MaxDiff(), 250, self.update)
-        cv2.createTrackbar('minDisparities', 'Disparity', self.stereo.getMinDisparity(), 250, self.update)
-        # cv2.createTrackbar('numDisparity', 'Disparity', self.stereo.getNumDisparities(), 250, self.update)
+        cv2.createTrackbar('minDisparity', 'Disparity', self.stereo.getMinDisparity(), 250, self.update)
+        cv2.createTrackbar('numDisparities', 'Disparity', self.stereo.getNumDisparities(), 250, self.update)
         cv2.createTrackbar('preFilterCap', 'Disparity', self.stereo.getPreFilterCap(), 250, self.update)
-        cv2.createTrackbar('P1', 'Disparity', self.stereo.getP1(), 10000, self.update)
-        cv2.createTrackbar('P2', 'Disparity', self.stereo.getP2(), 10000, self.update)
+        cv2.createTrackbar('P1', 'Disparity', self.stereo.getP1(), 1000, self.update)
+        cv2.createTrackbar('P2', 'Disparity', self.stereo.getP2(), 5000, self.update)
 
         self.update()
         cv2.waitKey()
