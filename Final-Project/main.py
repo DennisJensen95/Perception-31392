0f7e688e597637b115f52d0cfc52f17b39f7c229
Dennis Jensen <ngh1adh95@hotmail.com>
2020-04-23 11:12:57 +0200

    Testing rcnn pretrained

--- Final-Project/main.py
+++ Final-Project/main.py
@@ -5,6 +5,8 @@ from lib.construct3D import *
 import cv2
 from lib.sliderProgram import Slider
 import matplotlib.pyplot as plt
+import imutils
+from lib.fastRCNNPretrained import object_detection_api
 
 def main():
     images_left = sorted(glob.glob("data/Stereo_calibration_images/left*.png"))
@@ -16,6 +18,7 @@ def main():
 
     calibrate = False
     debug_disp_slider = False
+    preTrainedFastRcnn = False
 
     nb_vertical = 6
     nb_horizontal = 9
@@ -53,36 +56,56 @@ def main():
                                    preFilterCap=preFilter,
                                    mode=mode)
 
-    images_conveyor = images_conveyor[100:101]  # start from first box with 80:
+    images_conveyor = images_conveyor[800:1300]  # start from first box with 80:
+    objects_to_detect = ['cup', 'book']
+    down_sample_ratio = 0.4
+
+    ref_image_left = Cal.remapImagesStereo(images_conveyor[0], random=False, debug=False)[0]
+    ref_image_left = downsample_image(ref_image_left, down_sample_ratio)
+    # cv2.imshow('ref img', ref_image_left)
+    # cv2.waitKey(0)
 
     for images in images_conveyor:
         left_img, right_img = Cal.remapImagesStereo(images, random=False, debug=False)
         # images = np.concatenate((left_img, right_img), axis=1)
-        left_img = downsample_image(left_img, 0.4)
-        right_img = downsample_image(right_img, 0.4)
-        plt.imshow(left_img[110:130, 450:480])
-        plt.show()
+        left_img = downsample_image(left_img, down_sample_ratio)
+        right_img = downsample_image(right_img, down_sample_ratio)
+
         if debug_disp_slider:
             slider = Slider(stereo, left_img, right_img)
             slider.create_slider()
 
         disparity_img = stereo.compute(left_img, right_img).astype(np.float32) / 16.0
-        norm_disparity_img = cv2.normalize(disparity_img, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX,
-                                           dtype=cv2.CV_32F)
-        test_disp = disparity_img[110:130, 450:480]
-        Q_scaled = (Cal.Q*0.4)
-        z_map = construct_z_coordinate(test_disp, baseline, focal_length*0.4)
-        print(z_map)
+        # norm_disparity_img = cv2.normalize(disparity_img, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX,
+        #                                    dtype=cv2.CV_32F)
 
-        cv2.imshow('Images', left_img)
-        if cv2.waitKey(0) & 0xFF == ord('q'):
+        if preTrainedFastRcnn:
+            img_fastRCNN = object_detection_api(left_img, objects_to_detect, threshold=0.8)
+
+        difference = cv2.absdiff(left_img, ref_image_left)
+        _, thresh = cv2.threshold(difference, 50, 255, cv2.THRESH_BINARY)
+        thresh_gray = cv2.cvtColor(thresh, cv2.COLOR_BGR2GRAY)
+
+        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5,5))
+        mask = cv2.morphologyEx(thresh_gray, cv2.MORPH_OPEN, kernel, iterations=3)
+
+        contours = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        contours = np.asarray(imutils.grab_contours(contours))
+        cv2.drawContours(left_img, contours, -1, (0, 255, 0), 3)
+
+
+
+        # test_disp = disparity_img[110:130, 450:480]
+        # Q_scaled = (Cal.Q*0.4)
+        # z_map = construct_z_coordinate(test_disp, baseline, focal_length*down_sample_ratio)
+
+        cv2.imshow('Images', )
+        if cv2.waitKey(10) & 0xFF == ord('q'):
             break
-
-
 
     cv2.destroyAllWindows()
 
-    export_pointcloud(disparity_map=disparity_img, colors=left_img, Q=Q_scaled)
+    # export_pointcloud(disparity_map=disparity_img, colors=left_img, Q=Q_scaled)
 
 
 if __name__ == '__main__':
