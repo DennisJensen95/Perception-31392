f9be1074cc1253592bd712af5685ed87709ae801
theismunk <s164022@student.dtu.dk>
2020-04-23 10:30:44 +0200

    draw contour with centroid v1

--- Final-Project/lib/tracking.py
+++ Final-Project/lib/tracking.py
@@ -2,6 +2,27 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import imutils
+from numpy.linalg import inv, pinv
+
+
+"""
+FEATURE TRACKING
+"""
+
+
+def calculate_centroid(contours):
+    cnt = contours[-1]
+    M = cv2.moments(cnt)
+
+    cx = int(M['m10'] / M['m00'])
+    cy = int(M['m01'] / M['m00'])
+
+    return cx, cy
+
+
+"""
+KALMAN FILTER
+"""
 
 
 def update(x, P, Z, H, R):
--- Final-Project/main.py
+++ Final-Project/main.py
@@ -4,6 +4,7 @@ import imutils
 # from skimage.metrics import structural_similarity as ssim
 from lib.calibration import Calibration
 from lib.construct3D import *
+from lib.tracking import *
 import cv2
 from lib.sliderProgram import Slider
 
@@ -61,9 +62,9 @@ def main():
     images_conveyor = images_conveyor[0:-1]  # start from first box with 80:
 
     left_img, _ = Cal.remapImagesStereo(images_conveyor[0], random=False, debug=False)
-    reference_img = left_img  # reference image to be used for tracking
+    reference_img = cv2.GaussianBlur(left_img, (11, 11), 0)  # reference image to be used for tracking
 
-    for images in images_conveyor[150:]:
+    for images in images_conveyor[350:]:
         left_img, right_img = Cal.remapImagesStereo(images, random=False, debug=False)
         # images = np.concatenate((left_img, right_img), axis=1)
         # left_img = downsample_image(left_img, 0.5)
@@ -77,21 +78,23 @@ def main():
         # norm_disparity_img = cv2.normalize(disparity_img, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX,
         #                                    dtype=cv2.CV_32F)
 
-
-        difference = cv2.absdiff(left_img, reference_img)
+        left_img_blurred = cv2.GaussianBlur(left_img, (23, 23), 0)
+        difference = cv2.absdiff(left_img_blurred, reference_img)
         # diff_gray = cv2.cvtColor(difference, cv2.COLOR_BGR2GRAY)
         _, thresh = cv2.threshold(difference, 50, 255, cv2.THRESH_BINARY)
         thresh_gray = cv2.cvtColor(thresh, cv2.COLOR_BGR2GRAY)
 
         kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
         mask = cv2.morphologyEx(thresh_gray, cv2.MORPH_OPEN, kernel, iterations=3)
+        mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel, iterations=3)
         # cutout = cv2.cvtColor(left_img * mask[:, :, None], cv2.COLOR_BGR2RGB)
         contours = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        contours = imutils.grab_contours(contours)
-        cv2.drawContours(left_img, contours, -1, (0,255,0), 3)
+        contours = np.asarray(imutils.grab_contours(contours))
+        cv2.drawContours(left_img, contours, -1, (0, 255, 0), 3)
 
-
-
+        if len(contours) > 0:
+            cx, cy = calculate_centroid(contours)
+            cv2.circle(left_img, (cx, cy), 20, (255, 0, 0), -1)
 
 
         cv2.imshow('Images', left_img)
